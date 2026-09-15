feab34ac5a1e142f0f92a9fb365ec6f8838eff2f
Alissia Merkulova <alissia.merkulova@tptlive.ee>
2026-09-15 11:31:26 +0300

    move variables to public index

--- views/index.php
+++ views/index.php
@@ -1,35 +1,3 @@
-<?php
-$title = 'U.S';
-$posts = [
-  [
-    'title' => 'Some world title 1', 
-    'content' => 'Some world content 1', 
-    'date' => 'January 1, 2021', 
-    'author' => 'Pets'
-    ],
-    [
-    'title' => 'Some world title 2', 
-    'content' => 'Some world content 2', 
-    'date' => 'January 2, 2021', 
-    'author' => 'Alex'
-    ],
-    [
-    'title' => 'Some world title 3', 
-    'content' => 'Some world content 3', 
-    'date' => 'January 3, 2021', 
-    'author' => 'Juss'
-    ],
-    [
-    'title' => 'Some world title 4', 
-    'content' => 'Some world content 4', 
-    'date' => 'January 4, 2021', 
-    'author' => 'Ron'
-    ],
-];
-
-?>
-
-
 <?php include __DIR__ . '/partials/header.php'; ?>
     
     
