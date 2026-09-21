a6c07ccf01fdcd9f4151a3decc5fc1fea4f6f091
Alissia Merkulova <alissia.merkulova@tptlive.ee>
2026-09-21 09:15:35 +0300

    Move logic to controller

--- helpers.php
+++ helpers.php
@@ -4,4 +4,9 @@ function dump(...$vars) {
     echo '<pre>';
     var_dump(...$vars);
     echo '</pre>';
+    }
+
+function view($viewName, $variables=[]){
+    extract($variables);
+    include __DIR__ . "/views/$viewName.php";
 }
