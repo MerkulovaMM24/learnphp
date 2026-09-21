f7e12c99da4af529a2e7216bb80aadcd281bedc6
Alissia Merkulova <alissia.merkulova@tptlive.ee>
2026-09-21 08:57:36 +0300

    Add autoload and namespaces

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER['REQUEST_URI'])) {
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', 
+$_SERVER['REQUEST_URI'])) {
     return false;
 }
 function dump(...$vars) {
@@ -8,43 +9,82 @@ function dump(...$vars) {
     echo '</pre>';
 }
 
-switch($_SERVER['REQUEST_URI']) {
-    case '/':
-        include __DIR__ . '/../views/index.php';
-        break;
-    case '/us':
-        $title = 'U.S';
-        $posts = [
-        [
-            'title' => 'Some world title 1', 
-            'content' => 'Some world content 1', 
-            'date' => 'January 1, 2021', 
-            'author' => 'Pets'
-            ],
-            [
-            'title' => 'Some world title 2', 
-            'content' => 'Some world content 2', 
-            'date' => 'January 2, 2021', 
-            'author' => 'Alex'
-            ],
-            [
-            'title' => 'Some world title 3', 
-            'content' => 'Some world content 3', 
-            'date' => 'January 3, 2021', 
-            'author' => 'Juss'
-            ],
-            [
-            'title' => 'Some world title 4', 
-            'content' => 'Some world content 4', 
-            'date' => 'January 4, 2021', 
-            'author' => 'Ron'
-            ],
-        ];
-        include __DIR__ . '/../views/us.php';
-        break;
-    case '/tech':
-        include __DIR__ . '/../views/tech.php';
-        break;
-    default: 
-        echo '404';
-}
+spl_autoload_register(function ($class){
+    $class = substr($class,4);
+    $class = str_replace('\\', '/', $class);
+    require_once __DIR__ . "/../src/$class.php";
+});
+
+use App\Controllers\PublicController as PC;
+
+// require_once __DIR__ . '/../src/Router.php';
+// require_once __DIR__ . '/../src/DB.php';
+$controller = new PC();
+$router = new App\Router();
+$db = new App\DB();
+dump($router,$db);
+
+// switch ($_SERVER['REQUEST_URI']) {
+//     case '/':
+//         $title = 'World';
+//         $posts = [
+//             [
+//                 'title' => 'Some world title 1',
+//                 'content' => 'Some world content 1',
+//                 'date' => 'January 1, 2021',
+//                 'author' => 'Pets',
+//             ],
+//             [
+//                 'title' => 'Some world title 2',
+//                 'content' => 'Some world content 2',
+//                 'date' => 'January 2, 2021',
+//                 'author' => 'Juss',
+//             ],
+//             [
+//                 'title' => 'Some world title 3',
+//                 'content' => 'Some world content 3',
+//                 'date' => 'January 3, 2021',
+//                 'author' => 'Alex',
+//             ],
+//             [
+//                 'title' => 'Some world title 4',
+//                 'content' => 'Some world content 4',
+//                 'date' => 'January 4, 2021',
+//                 'author' => 'Manivald',
+//             ],
+//         ];
+//         include __DIR__ . '/../views/index.php';
+//         break;
+//     case '/us':
+//         $title = 'U.S';
+//         $posts = [
+//             [
+//                 'title' => 'Some U.S title 1',
+//                 'content' => 'Some U.S content 1',
+//                 'date' => 'January 1, 2021',
+//                 'author' => 'Pets',
+//             ],
+//             [
+//                 'title' => 'Some U.S title 2',
+//                 'content' => 'Some U.S content 2',
+//                 'date' => 'January 2, 2021',
+//                 'author' => 'Juss',
+//             ],
+//             [
+//                 'title' => 'Some U.S title 3',
+//                 'content' => 'Some U.S content 3',
+//                 'date' => 'January 3, 2021',
+//                 'author' => 'Alex',
+//             ],
+//             [
+//                 'title' => 'Some U.S title 4',
+//                 'content' => 'Some U.S content 4',
+//                 'date' => 'January 4, 2021',
+//                 'author' => 'Manivald',
+//             ],
+//         ];
+//         include __DIR__ . '/../views/us.php';
+//         break;
+//     default:
+//         echo '404';
+// }
